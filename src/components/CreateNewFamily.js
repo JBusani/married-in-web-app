1917fb19b5e2fbb82d5c062b923add446c06f0d0
Jake Bersani <53477487+JBusani@users.noreply.github.com>
2022-03-19 00:33:37 +0900

    COMPLETED: logic for adding existing members already in Firestore DB to new family form. CHORE: include id

--- src/components/CreateNewFamily.js
+++ src/components/CreateNewFamily.js
@@ -1,53 +1,78 @@
 import React, { useRef, useState, useEffect } from 'react'
-import { collection, addDoc, setDoc, doc, documentId } from "firebase/firestore"; 
+import { collection, query, where , getDocs, addDoc, setDoc, doc, documentId } from "firebase/firestore"; 
 import { db } from './Firebase';
 import formStyles from "./form.module.css";
-import MemberFieldSet from './form/memberFieldset';
-/*
-members : [
-    id : {
-      firstName: Jake
-      lastName: Bersani
-      image: *.jpg
-      }
-    ]
+import {MemberFieldSet, ExistingMemberFieldSet} from './form/memberFieldset';
 
-
-*/
 
 export default function CreateNewFamily(props){
     const familyName = useRef();
-    const [ memberArray, setMemberArray ] = useState([<MemberFieldSet key={1} memberNumber={1} />]);
+    //memberCollection is an array of objects
+    const [ memberCollection, setMemberCollection ] = useState([]);
+    const [ filteredCollection, setFilteredCollection ] = useState([]);
+    const [ fieldMemberArray, setFieldMemberArray ] = useState([<MemberFieldSet key={1} memberNumber={1} />]);
+    const [ existingMember, setExistingMember ] = useState(null)
+    const [ openSearchExisting, setOpenSearchExisting ] = useState(false)
+    const [ disableButton, setDisableButton ] = useState(false)
 
-    function handleAddMember(event){
+    function handleAddMember(){
       //create a new fieldset array
-      setMemberArray([
-        ...memberArray,
-        <MemberFieldSet key={memberArray.length + 1} memberNumber={memberArray.length + 1} />
-      ])
+        setFieldMemberArray(prevState => ([
+          ...prevState,
+          <MemberFieldSet key={fieldMemberArray.length + 1} memberNumber={fieldMemberArray.length + 1} />
+        ]))
+    }
+    function handleAddExistingMember(){
+      setDisableButton(true);
+      setOpenSearchExisting(true);
     }
 
+    useEffect(async ()=>{
+      const querySnapshot = await getDocs(collection(db, `users/${props.user}/members`));
+      const dbMembers = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+          dbMembers.push({
+            id: doc.id,
+            firstName: doc.data().firstName,
+            familyName: doc.data().familyName,
+            families: doc.data().families
+          });
+      });
+      setMemberCollection(dbMembers)
 
+      async function queryByFamiliesArray(){
+        const f = query(collection(db, `users/${props.user}/members`),where("families", "array-contains", "Bersani"));
+        const fSnap = await getDocs(f);
+        fSnap.forEach((doc)=>{
+          console.log(doc.data())
+        })
+      }
+      queryByFamiliesArray();
+
+    },[])
     async function handleSubmit(event){  
       event.preventDefault();
       const formData = event.target;
-
+      console.group("running submit function", formData)
+      
+      try{
       const formDataFormated = () => {
         const familyMembers = formData.querySelectorAll("fieldset")
         let familyMembersArray = [];
-        const membersCount = memberArray.length;
+        const membersCount = fieldMemberArray.length;
         
-        //fieldset members formatted array. how many members.
+        //return all fieldsets from form into familyMemberArray
+        //each as an family member object
         for(let i = 0; i < membersCount; i++){
           const fields = familyMembers[i];
           const inputFields = fields.querySelectorAll("input")
           let familyMember = {
             firstName: "",
             parentRole: false,
-            childRole: false
+            childRole: false,
           }
           inputFields.forEach(input => {
-            console.log(inputFields.length)
             switch(input.id){
               case "firstName":
                 familyMember.firstName = input.value;
@@ -60,18 +85,30 @@ export default function CreateNewFamily(props){
             }
           });
           familyMembersArray.push(familyMember);
-          
+          console.group("for loop finished. family members array complete");
         }//for loop finished
-        console.group(familyMembersArray)
         return familyMembersArray
       }
       formDataFormated();
+      console.group("finished running formDataFormated function", formDataFormated());
+    }catch(error){
+      console.group(error);
+    }
         /*try{
           //to create or overwrite a single document use set()
           //if not exist, it'll be created
           //if exist it will be overwritten with the newly provided data unless you specify it should be merged.
 
-          
+          //in the families collection, add a family with this data object
+          const membersRef = await addDoc(collection(db, `users/${props.user}/members`),{
+            familyName: familyName.current.value,
+            firstName: "",
+            families: [
+              familyName.current.value
+            ],
+          })
+          console.log(membersRef)
+
           //in the families collection, add a family with this data object
           const familyRef = await addDoc(collection(db, `users/${props.user}/families`), {
             family: familyName.current.value,
@@ -81,16 +118,18 @@ export default function CreateNewFamily(props){
           })
           console.log(familyRef)
 
-          //in the families collection, add a family with this data object
-          const membersRef = await addDoc(collection(db, `users/${props.user}/members`),{
-            familyName: familyName.current.value,
-            firstName: "",
-          })
-          console.log(membersRef)
+          
         }catch(error){
           console.group(error)
         }*/
 
+    }
+    
+    function checkForMember(event){
+      const fname = event.target.value.toLowerCase();
+      const filterResult = memberCollection.filter(member => member.firstName.toLowerCase() === fname);
+      setFilteredCollection(filterResult)
+      return filterResult;
     }
     return (
         <div className={formStyles.formContainer}>
@@ -105,18 +144,56 @@ export default function CreateNewFamily(props){
                 />
                 <p>Members</p>
                 <div id='members'>
-                  {memberArray.map(member=> {
+                  {fieldMemberArray.map(member=> {
                     return member;
                   })}
                 </div>
                 <button
+                id='addNewMember'
                 onClick={handleAddMember} 
                   type='button'
                   className={formStyles.submitButton}
                   >
-                    Add Member
+                    Add New Member
+                </button>
+                {
+                  openSearchExisting ? (
+                  <div>
+                    <input type="text" id="search" onChange={checkForMember}/>
+                    <ul style={{listStyle: "none"}}>
+                      {filteredCollection.map((m, index) => {
+                        function addExistingMember(){
+                          setFieldMemberArray(prevState => ([
+                            ...prevState,
+                            <ExistingMemberFieldSet value={m} key={fieldMemberArray.length + 1} memberNumber={fieldMemberArray.length + 1} />
+                          ]))
+                          setOpenSearchExisting(false);
+                          setDisableButton(false)
+                        }
+                        return (
+                          <li type='button' onClick={addExistingMember} key={m.id} style={{border: "solid black 2px",  padding: "10px", marginBottom: "20px", cursor: "pointer"}}>
+                            <p>
+                              First Name: {m.firstName} </p>
+                            <p>Belonging to: <br/>{m.families}</p>
+                            <p>ID: {m.id} </p>
+                          </li>
+                        )  
+                      })}
+                    </ul>
+                  </div>
+                ) : null}
+                
+                <button
+                  id='addExistingMember'
+                  onClick={handleAddExistingMember} 
+                  disabled={disableButton}
+                  type='button'
+                  className={formStyles.submitButton}
+                  >
+                    Add Existing Member
                 </button>
                 <button
+                    disabled={disableButton}
                     id="submit"
                     type="submit"
                     className={formStyles.submitButton}>
@@ -125,4 +202,4 @@ export default function CreateNewFamily(props){
                 </form>  
             </div>
     );
-}
+} 
--- src/components/form/memberFieldset.js
+++ src/components/form/memberFieldset.js
@@ -1,7 +1,9 @@
-import React, {useRef} from 'react';
+import React, {useState} from 'react';
 
 const MemberFieldSet = ({memberNumber}) => {
-
+//may need state to replace values if includes in membercollection on change.
+//then need to update that members families field with the family name
+//need to update family with member name
     return (
         <>
         <fieldset id={`member${memberNumber}`} name={`member${memberNumber}`}>
@@ -19,7 +21,7 @@ const MemberFieldSet = ({memberNumber}) => {
                       <input
                         type="radio"
                         id='parentRole'
-                        name={`member${memberNumber}`}
+                        name={`memberRole${memberNumber}`}
                         required={true}
                       />
                     </label>
@@ -27,7 +29,7 @@ const MemberFieldSet = ({memberNumber}) => {
                       <input
                         type="radio"
                         id='childRole'
-                        name={`member${memberNumber}`}
+                        name={`memberRole${memberNumber}`}
                         required={true}
                       />
                     </label>
@@ -37,4 +39,47 @@ const MemberFieldSet = ({memberNumber}) => {
     )
 }
 
-export default MemberFieldSet;
+const ExistingMemberFieldSet = ({memberNumber, value}) => {
+  //may need state to replace values if includes in membercollection on change.
+  //then need to update that members families field with the family name
+  //need to update family with member name
+      return (
+          <>
+          <fieldset id={value.id} name={`member${memberNumber}`}>
+              <legend>Member{memberNumber}</legend>
+                    <input 
+                      type="text" 
+                      id='firstName'
+                      placeholder="First Name" 
+                      name={`firstNameMember${memberNumber}`}
+                      aria-label="first Name"
+                      required={true}
+                      disabled={true}
+                      value={value.firstName}
+                    />
+                    <div>
+                      <label>Parent:
+                        <input
+                          type="radio"
+                          id='parentRole'
+                          name={`memberRole${memberNumber}`}
+                          required={true}
+                        />
+                      </label>
+                      <label>Child:
+                        <input
+                          type="radio"
+                          id='childRole'
+                          name={`memberRole${memberNumber}`}
+                          required={true}
+                        />
+                      </label>
+                    </div>
+                  </fieldset>
+          </>
+      )
+  }
+export {
+  MemberFieldSet,
+  ExistingMemberFieldSet
+}
